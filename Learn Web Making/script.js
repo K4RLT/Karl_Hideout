896b3e4914c0f1d1f08cd70471295084c8f5ed31
Karl <107928712+K4RLT@users.noreply.github.com>
2025-03-16 02:12:37 +0800

    Delete Learn Web Making directory

--- Learn Web Making/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function toggleMenu() {
-        var menu = document.querySelector(".menu");
-        menu.style.display = (menu.style.display === "block") ? "none" : "block";
-    }
-
-    document.querySelector(".menu-icon").addEventListener("click", toggleMenu);
-});
